99500328d59e4470474e8c76b43766283d379c30
rafi ullah <90065622+rafiullah786786@users.noreply.github.com>
2022-10-22 06:12:27 -0700

    some changes again

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -22,33 +22,6 @@ function Home() {
             ipsum dolor ea similique. Quos repudiandae exercitationem odio
             placeat autem in, cupiditate at ipsa assumenda rerum laudantium
             aspernatur amet, quod quibusdam sit! Excepturi, iusto! Asperiores
-            eaque veritatis voluptatibus ad eum recusandae sed, neque excepturi
-            fuga nemo atque molestiae repudiandae deserunt error itaque ut nobis
-            consequuntur id voluptas nisi beatae voluptate. Sed ea dicta
-            voluptas? Impedit dolorum pariatur doloribus sint. Error
-            repudiandae, eos quae nam mollitia voluptate pariatur quaerat aut
-            dolores blanditiis impedit fugiat deserunt, architecto tenetur
-            suscipit quidem ab placeat commodi possimus magnam totam corrupti,
-            rem eveniet a. Debitis itaque, magni doloremque architecto sequi
-            animi illum excepturi quo, illo nostrum sit est libero delectus,
-            deleniti unde exercitationem consectetur quia explicabo aut.
-            Repellat alias illo saepe sit iure perspiciatis corporis nam,
-            suscipit officia accusamus. Modi a nulla vero facere corrupti quam
-            fugit debitis quod dolorem omnis sunt, mollitia repellat dolor
-            assumenda. Mollitia vitae repellendus odio alias rem libero quaerat
-            sed minus, dolore voluptas, exercitationem esse totam quia deserunt
-            deleniti! Velit porro saepe tempore repudiandae voluptatem dolor
-            suscipit rerum, provident illo rem, assumenda ratione accusantium
-            ipsum necessitatibus consequuntur architecto temporibus, eos
-            perferendis ab illum? Itaque ratione vitae numquam harum fugit? Non,
-            iste harum! Lorem ipsum dolor sit amet, consectetur adipisicing
-            elit. Laborum odio praesentium officia, eos deleniti sit molestias
-            ut pariatur eius voluptates sed nihil autem aliquid at fuga labore
-            ab ducimus numquam sint doloribus accusamus similique cum magnam
-            excepturi. imus ad minima, corporis, dolorem, repellat ratione
-            libero cupiditate unde praesentium. Impedit tenetur delectus
-            aspernatur laborum illum quae obcaecati praesentium voluptatem
-            provident at veniam numquam ipsum sequi, nostrum nobis, optio ex ea.
           </p>
         </div>
       </div>
